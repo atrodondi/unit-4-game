ea9da8c5add95c17e5b4fbfb769cf3f13d56539e
cheebachong <atrodondi@gmail.com>
2020-02-12 00:54:39 -0800

    getting the look down and divs set up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,20 @@
-var luke;
-var senator;
-var obiwan;
-var vader;
+var luke = {
+  Attack: 8,
+  Health: 110,
+  counterAttack: 10
+};
+var senator = {
+  Attack: 7,
+  Health: 100,
+  counterAttack: 16
+};
+var obiwan = {
+  Attack: 5,
+  Health: 105,
+  counterAttack: 18
+};
+var vader = {
+  Attack: 6,
+  Health: 120,
+  counterAttack: 15
+};
