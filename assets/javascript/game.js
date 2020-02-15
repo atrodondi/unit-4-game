08ab265a32b488695a99474a8c84f4e585fab359
cheebachong <atrodondi@gmail.com>
2020-02-14 16:28:57 -0800

    added functions for choosing heroes and moving the others to their new areas

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,8 +20,54 @@ var vader = {
 };
 
 $(document).ready(function() {
+  // this displays all heroes HP to their "card"
   $("#lukeHP").text(luke.Health);
   $("#obiwanHP").text(obiwan.Health);
   $("#emperorHP").text(emperor.Health);
   $("#vaderHP").text(vader.Health);
+
+  // if Luke gets picked as hero, moves him to your hero div and
+  // moves other chars to enemy div and alters CSS
+  $("#lukeBox").on("click", function() {
+    $("#lukeBox").appendTo("#yourHero");
+    $("#obiwanBox").appendTo("#enemies");
+    $("#obiwanBox").toggleClass("enemy");
+    $("#emperorBox").appendTo("#enemies");
+    $("#emperorBox").toggleClass("enemy");
+    $("#vaderBox").appendTo("#enemies");
+    $("#vaderBox").toggleClass("enemy");
+  });
+
+  // if Obiwan is chosen, sends others to enemy etc.
+  $("#obiwanBox").on("click", function() {
+    $("#obiwanBox").appendTo("#yourHero");
+    $("#lukeBox").appendTo("#enemies");
+    $("#lukeBox").toggleClass("enemy");
+    $("#emperorBox").appendTo("#enemies");
+    $("#emperorBox").toggleClass("enemy");
+    $("#vaderBox").appendTo("#enemies");
+    $("#vaderBox").toggleClass("enemy");
+  });
+
+  // if Emperor is chosen, etc.
+  $("#emperorBox").on("click", function() {
+    $("#emperorBox").appendTo("#yourHero");
+    $("#lukeBox").appendTo("#enemies");
+    $("#lukeBox").toggleClass("enemy");
+    $("#vaderBox").appendTo("#enemies");
+    $("#vaderBox").toggleClass("enemy");
+    $("#obiwanBox").appendTo("#enemies");
+    $("#obiwanBox").toggleClass("enemy");
+  });
+
+  // if Vader is chosen
+  $("#vaderBox").on("click", function() {
+    $("#vaderBox").appendTo("#yourHero");
+    $("#lukeBox").appendTo("#enemies");
+    $("#lukeBox").toggleClass("enemy");
+    $("#obiwanBox").appendTo("#enemies");
+    $("#obiwanBox").toggleClass("enemy");
+    $("#emperorBox").appendTo("#enemies");
+    $("#emperorBox").toggleClass("enemy");
+  });
 });
