e34bcfc0aae98486a08a56bd934662060e9da260
cheebachong <atrodondi@gmail.com>
2020-02-15 23:57:22 -0800

    changing hp and stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,39 @@
 var luke = {
   Attack: 8,
+  BaseAttack: 8,
   Health: 110,
   counterAttack: 10
 };
 var emperor = {
   Attack: 7,
+  BaseAttack: 7,
   Health: 100,
   counterAttack: 16
 };
 var obiwan = {
   Attack: 5,
+  BaseAttack: 5,
   Health: 105,
   counterAttack: 18
 };
 var vader = {
   Attack: 6,
+  BaseAttack: 6,
   Health: 120,
   counterAttack: 15
 };
-
-$(document).ready(function() {
-  // this displays all heroes HP to their "card"
+var myHero;
+var myDefender;
+function updateHP() {
   $("#lukeHP").text(luke.Health);
   $("#obiwanHP").text(obiwan.Health);
   $("#emperorHP").text(emperor.Health);
   $("#vaderHP").text(vader.Health);
+}
+
+$(document).ready(function() {
+  // this displays all heroes HP to their "card"
+  updateHP();
 
   // if Luke gets picked as hero, moves him to your hero div and
   // moves other chars to enemy div and alters CSS
@@ -33,6 +42,8 @@ $(document).ready(function() {
     if ($("#lukeBox", "#start").length == 1) {
       // this is sending the clicked hero to chosen place and other guys to enemies
       $("#lukeBox").appendTo("#yourHero");
+      myHero = luke;
+      console.log(myHero);
       $("#obiwanBox")
         .appendTo("#enemies")
         .toggleClass("enemy");
@@ -49,7 +60,10 @@ $(document).ready(function() {
       if ($("#defender").children().length == 1) {
         $("#lukeBox").appendTo("#defender");
         $("#lukeBox").toggleClass("defense");
+        myDefender = luke;
+        $("#lukeHP").text(myDefender.Health);
       }
+      return myHero;
     }
   });
 
@@ -57,18 +71,24 @@ $(document).ready(function() {
   $("#obiwanBox").on("click", function() {
     if ($("#obiwanBox", "#start").length == 1) {
       $("#obiwanBox").appendTo("#yourHero");
-      $("#lukeBox").appendTo("#enemies");
-      $("#lukeBox").toggleClass("enemy");
-      $("#emperorBox").appendTo("#enemies");
-      $("#emperorBox").toggleClass("enemy");
-      $("#vaderBox").appendTo("#enemies");
-      $("#vaderBox").toggleClass("enemy");
+      myHero = obiwan;
+      $("#lukeBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
+      $("#emperorBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
+      $("#vaderBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
     } else if ($("#obiwanBox", "#enemies").length == 1) {
       // ok figured it out, the title Defender, Duh! so set the check to 1 since that will always be there ,
       // then append the hero to it, it will stay til dead and gone
       if ($("#defender").children().length == 1) {
         $("#obiwanBox").appendTo("#defender");
         $("#obiwanBox").toggleClass("defense");
+        myDefender = obiwan;
+        $("#obiwanHP").text(myDefender.Health);
       }
     }
   });
@@ -77,16 +97,22 @@ $(document).ready(function() {
   $("#emperorBox").on("click", function() {
     if ($("#emperorBox", "#start").length == 1) {
       $("#emperorBox").appendTo("#yourHero");
-      $("#lukeBox").appendTo("#enemies");
-      $("#lukeBox").toggleClass("enemy");
-      $("#vaderBox").appendTo("#enemies");
-      $("#vaderBox").toggleClass("enemy");
-      $("#obiwanBox").appendTo("#enemies");
-      $("#obiwanBox").toggleClass("enemy");
+      myHero = emperor;
+      $("#lukeBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
+      $("#vaderBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
+      $("#obiwanBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
     } else if ($("#emperorBox", "#enemies").length == 1) {
       if ($("#defender").children().length == 1) {
         $("#emperorBox").appendTo("#defender");
         $("#emperorBox").toggleClass("defense");
+        myDefender = emperor;
+        $("#emperorHP").text(myDefender.Health);
       }
     }
   });
@@ -95,17 +121,33 @@ $(document).ready(function() {
   $("#vaderBox").on("click", function() {
     if ($("#vaderBox", "#start").length == 1) {
       $("#vaderBox").appendTo("#yourHero");
-      $("#lukeBox").appendTo("#enemies");
-      $("#lukeBox").toggleClass("enemy");
-      $("#obiwanBox").appendTo("#enemies");
-      $("#obiwanBox").toggleClass("enemy");
-      $("#emperorBox").appendTo("#enemies");
-      $("#emperorBox").toggleClass("enemy");
+      myHero = vader;
+      $("#lukeBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
+      $("#obiwanBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
+      $("#emperorBox")
+        .appendTo("#enemies")
+        .toggleClass("enemy");
     } else if ($("#vaderBox", "#enemies").length == 1) {
       if ($("#defender").children().length == 1) {
         $("#vaderBox").appendTo("#defender");
         $("#vaderBox").toggleClass("defense");
+        myDefender = vader;
+        $("#vaderHP").text(vader.Health);
       }
     }
+  });
+
+  // if Attack button is pressed.
+  $("#attack").on("click", function() {
+    console.log(myHero.Attack);
+    myDefender.Health -= myHero.Attack;
+    myHero.Attack += myHero.BaseAttack;
+    myHero.Health -= myDefender.counterAttack;
+    console.log(myDefender.Health);
+    updateHP();
   });
 });
